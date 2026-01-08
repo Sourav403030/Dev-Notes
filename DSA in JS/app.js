302403cbe0d662f072290b6ea77b65ac8822b397
Sourav Sanjay <souravsanjay07@gmail.com>
2026-01-08 12:17:57 +0530

    One question added to strings.

---
+++ DSA in JS/app.js
@@ -0,0 +1,22 @@
+// Practise File
+
+// Good Number
+
+// function goodNumber(str){
+//     let result = "";
+//     for(let i = 0; i < str.length; i++){
+//         if(str[i] === str[i+1] && str[i] === str[i+2]){
+//             let candidate = str.slice(i,i+3);
+
+//             if(candidate > result){
+//                 result = candidate;
+//             }
+//         }
+//     }
+
+//     console.log(result);
+// }
+
+// goodNumber("6777133339");
+
+// goodNumber("2300019");
